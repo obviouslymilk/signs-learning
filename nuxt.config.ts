d3a4597fb7531d8f99e03120aac98809bf3ffc85
Klim <storozhenko.klim@gmail.com>
2025-12-15 19:57:03 +0300

    pkg: устанавливает eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
+
   vite: {
     plugins: [
       tailwindcss()
     ]
-  }
+  },
+
+  modules: ['@nuxt/eslint']
 })
