e72d6a00c752aa3bb0e816970999c38779121447
Klim <storozhenko.klim@gmail.com>
2025-12-17 22:11:57 +0300

    chore(sign): переименовывает категорию prohibitory на restrictive

--- shared/types/sign.ts
+++ shared/types/sign.ts
@@ -1,4 +1,4 @@
-export type SignCategory = 'warning' | 'priority' | 'prohibitory' | 'mandatory' | 'special' | 'information' | 'service' | 'additional';
+export type SignCategory = 'warning' | 'priority' | 'restrictive' | 'mandatory' | 'special' | 'information' | 'service' | 'additional';
 
 export type Sign = {
   rulesNumber: string
